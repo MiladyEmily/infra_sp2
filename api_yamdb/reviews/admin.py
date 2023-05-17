c64eac1eee51fb6a38413c5f0685e2cefbc00bf4
Emilia Chernova <emiliia.chernova@yandex.ru>
2023-05-17 22:46:24 +0300

    Спринт 15. Docker-контейнер для Yamdb

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 
 from .models import Comment, Review, Title, Genre, Category, User
 
-
-User = get_user_model()
 
 admin.site.register(User)
 admin.site.register(Genre)
