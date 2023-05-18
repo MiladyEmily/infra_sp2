e9704b0389e35cb7c42cfe915eac427913ec2d31
Эмилия Чернова <114020140+MiladyEmily@users.noreply.github.com>
2023-05-18 20:53:44 +0300

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', default="123456789")
+SECRET_KEY = os.getenv('SECRET_KEY', default='123456789')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
